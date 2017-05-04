2a0c11003fef34ee89eaede86ab936c1c6b93bb1
Theresa Mostert <theresa@isogramconsulting.com>
2017-05-04 19:46:09 +0200

    updated postgres functionality

--- node/src/server.js
+++ node/src/server.js
@@ -11,6 +11,18 @@ let votes = {
 	tacos: 0	
 };
 
+let client = new pg.Client('postgres://postgres@172.17.0.1:9000/postgres');
+
+client.connect(function (err){
+    if (err) throw err;
+    client.query('SELECT number_of_votes FROM votes', function (err, result){
+    if (err) throw err;
+        
+        votes.sandwiches = result.rows[0].number_of_votes;
+        votes.tacos = result.rows[1].number_of_votes;
+    });
+});
+
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
@@ -28,8 +40,14 @@ app.post('/vote', urlencodedParser, function (req, res) {
     if (vote === 'sandwiches'){
         
         votes.sandwiches = votes.sandwiches + 1;
+         client.query('UPDATE votes set number_of_votes=' + votes.sandwiches + 'where option_name=\'sandwiches\'', function (err, result){
+            if (err) throw err;
+        });
     } else if (vote === 'tacos'){
         votes.tacos = votes.tacos +1;
+         client.query('UPDATE votes set number_of_votes=' + votes.tacos + 'where option_name=\'tacos\'', function (err, result){
+            if (err) throw err;
+        });
     } else {
         console.log('Something went wrong' + vote);
     }
